f876511940cd9b627a207a0ae52e1522a3c897a5
bob <anishsubedi862@gmail.com>
2019-03-11 12:19:56 +0545

    final change

--- src/shared/dishes.js
+++ src/shared/dishes.js
@@ -3,10 +3,6 @@ export const DISHES = [
     id: 0,
     name: "Introduction to Habitat",
     image: "/assets/images/2.png",
-    category: "mains",
-    label: "Hot",
-    price: "4.99",
-    featured: true,
     description:
       "Sitecore Habitat is a range of sites demonstarting the capabilities of the Sitecore Experience Platform. The solution is built on the Sitecore Helix guidelines, which focus on increasing productivity and quality in Sitecore projects."
   },
@@ -14,10 +10,6 @@ export const DISHES = [
     id: 1,
     name: "Modules",
     image: "/assets/images/3.png",
-    category: "appetizer",
-    label: "",
-    price: "1.99",
-    featured: false,
     description:
       "The Habit example implementation has a range of modules which covers the basics of a Sitecore website."
   },
@@ -25,10 +17,6 @@ export const DISHES = [
     id: 2,
     name: "Getting Started",
     image: "/assets/images/4.png",
-    category: "appetizer",
-    label: "New",
-    price: "1.99",
-    featured: false,
     description:
       "Sitecore Helix a defined methodology with conventions and practises -Habitat is an example implementation available for your understanding."
   }
